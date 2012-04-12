e144c5bc1ff3607da638c2eaaa3277e11a45a768
Henry Neo <neohenry@gmail.com>
2012-04-12 22:14:47 +0800

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IMSPoc::Application.config.secret_token = '05855ab6addc2802350e2ca3a677934e3143f301a11d27cb02e5c585b6a08cb0490236db2d91d7974e0d204b987af158db9fcbef6ec7e95c4a6db0b0e815af83'
